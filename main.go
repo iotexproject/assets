68000e97eaf262172e9511358c285b01514d6087
ququzone <xueping.yang@gmail.com>
2021-10-10 10:26:19 +0800

    complete token detail

--- main.go
+++ main.go
@@ -31,11 +31,28 @@ func main() {
 		return c.SendString(string(data))
 	})
 
-	app.Get("", func(c *fiber.Ctx) error {
-		return nil
+	// GET /token/:chain/:address
+	app.Get("/token/:chain/:address", func(c *fiber.Ctx) error {
+		chain := c.Params("chain")
+		if strings.HasPrefix(chain, "/") || strings.HasPrefix(chain, "./") || strings.HasPrefix(chain, "..") {
+			return c.Status(http.StatusBadRequest).SendString("forbid prefix")
+		}
+		address := strings.ToLower(c.Params("address"))
+		data, err := ioutil.ReadFile("./blockchains/" + chain + "/assets/" + address + "/info.json")
+		if err != nil {
+			return c.Status(http.StatusBadRequest).SendString("unsupported chain or token")
+		}
+		c.Context().Response.Header.SetContentType(fiber.MIMEApplicationJSON)
+
+		return c.SendString(string(data))
 	})
 
-	app.Get("", func(c *fiber.Ctx) error {
+	// GET /token/:chain/:address/image/:tokenId
+	app.Get("token/:chain/:address/image/:tokenId", func(c *fiber.Ctx) error {
+		chain := c.Params("chain")
+		if strings.HasPrefix(chain, "/") || strings.HasPrefix(chain, "./") || strings.HasPrefix(chain, "..") {
+			return c.Status(http.StatusBadRequest).SendString("forbid prefix")
+		}
 		return nil
 	})
 
